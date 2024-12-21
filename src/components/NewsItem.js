42962c2dc229f2d9f1fd85801e9085af0c93d04c
subhumardi07 <subhashmardi8@gamil.com>
2024-12-21 16:26:02 +0530

    author and time with GMT is updated

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
-      let {title, description, imageUrl, newsUrl}= this.props;
+      let {title, description, imageUrl, newsUrl ,author, date}= this.props;
     return (
         <div className='my-3'>
             <div className="card" >
@@ -10,6 +10,7 @@ export class NewsItem extends Component {
               <div className="card-body">
                 <h5 className="card-title">{title}...</h5>
                 <p className="card-text">{description}...</p>
+                <p className="card-text"><small classname='tex-muted'>By {author ? author: "unknown" } on {new Date(date).toGMTString()}</small> </p>
                 <a href={newsUrl} target='_blank' rel="noreferrer" className="btn btn-dark btn-sm">Read More</a>
               </div>
             </div>
