a0f8d4031bab8a41df833e8f8fb58d0db477773e
subhumardi07 <subhashmardi8@gamil.com>
2024-12-23 16:59:33 +0530

    small mistake is corrected

--- src/App.js
+++ src/App.js
@@ -12,20 +12,20 @@ export default class App extends Component {
   render() {
     return (
       <div>
-      <Router>
+        <Router>
           <Navbar/>
             <Routes>
-                <Route exact path='/' element={<News key='general' pageSizes={this.pageSizes} country='us' category='general'/>} />
-                <Route exact path='/business' element={<News key='business' pageSizes={this.pageSizes} country='us' category='business'/>} />
-                <Route exact path='/entertainment' element={<News key='entertainment' pageSizes={this.pageSizes} country='us' category='entertainment'/>} />
-                <Route exact path='/general' element={<News key='general' pageSizes={this.pageSizes} country='us' category='general'/>} />
-                <Route exact path='/health' element={<News key='health' pageSizes={this.pageSizes} country='us' category='health'/>} />
-                <Route exact path='/science' element={<News key='science' pageSizes={this.pageSizes} country='us' category='science'/>} />
-                <Route exact path='/sports' element={<News key='sports' pageSizes={this.pageSizes} country='us' category='sports'/>} />
-                <Route exact path='/technology' element={<News key='technology' pageSizes={this.pageSizes} country='us' category='technology'/>} />
+                <Route path='/' element={<News key='general' pageSizes={this.pageSizes} country='us' category='general'/>} />
+                <Route path='/business' element={<News key='business' pageSizes={this.pageSizes} country='us' category='business'/>} />
+                <Route path='/entertainment' element={<News key='entertainment' pageSizes={this.pageSizes} country='us' category='entertainment'/>} />
+                <Route path='/general' element={<News key='general' pageSizes={this.pageSizes} country='us' category='general'/>} />
+                <Route path='/health' element={<News key='health' pageSizes={this.pageSizes} country='us' category='health'/>} />
+                <Route path='/science' element={<News key='science' pageSizes={this.pageSizes} country='us' category='science'/>} />
+                <Route path='/sports' element={<News key='sports' pageSizes={this.pageSizes} country='us' category='sports'/>} />
+                <Route path='/technology' element={<News key='technology' pageSizes={this.pageSizes} country='us' category='technology'/>} />
             </Routes>
-      </Router>
+        </Router>
       </div>
-    )
+    );
   }
 }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,14 +6,14 @@ export class NewsItem extends Component {
     return (
         <div className='my-3'>
             <div className="card" >
-              <img src={!imageUrl?" https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg":imageUrl} className="card-img-top" alt="..."/>
+              <img src={!imageUrl?" https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg":imageUrl} className="card-img-top" alt="news"/>
               <div className="card-body">
                 <h5 className="card-title">{title}   
                     <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{zIndex:'1',left:'84%'}}>{source}
                     </span>
                 </h5>
                 <p className="card-text">{description}...</p>
-                <p className="card-text"><small className='tex-muted'>By {author ? author: "unknown" } on {new Date(date).toGMTString()}</small> </p>
+                <p className="card-text"><small className='text-muted'>By {author ? author: "unknown" } on {new Date(date).toGMTString()}</small> </p>
                 <a href={newsUrl} target='_blank' rel="noreferrer" className="btn btn-dark btn-sm">Read More</a>
               </div>
             </div>
