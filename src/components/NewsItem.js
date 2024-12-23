122d677456c6be1da8070d674c3e2492c53eac53
subhumardi07 <subhashmardi8@gamil.com>
2024-12-23 11:08:41 +0530

    source is updated
    name of source is updated on the top right corner of heading

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
-      let {title, description, imageUrl, newsUrl ,author, date}= this.props;
+      let {title, description, imageUrl, newsUrl ,author, date, source}= this.props;
     return (
         <div className='my-3'>
             <div className="card" >
               <img src={!imageUrl?" https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg":imageUrl} className="card-img-top" alt="..."/>
               <div className="card-body">
-                <h5 className="card-title">{title}...</h5>
+                <h5 className="card-title">{title}   
+                    <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{zIndex:'1',left:'84%',}}>{source}
+                    </span>
+                </h5>
                 <p className="card-text">{description}...</p>
                 <p className="card-text"><small classname='tex-muted'>By {author ? author: "unknown" } on {new Date(date).toGMTString()}</small> </p>
                 <a href={newsUrl} target='_blank' rel="noreferrer" className="btn btn-dark btn-sm">Read More</a>
