297b4a29bf0cd13e0ec1b4d4a698e3d5f20ecbc4
subhumardi07 <subhashmardi8@gamil.com>
2024-12-27 12:20:00 +0530

    Hiding API Key
    Hiding API Key by Adding Custom Environment Variables

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import LoadingBar from 'react-top-loading-bar';
 
 export default class App extends Component {
   pageSizes=5;
+  apiKey=process.env.REACT_APP_NEWS_API
   
   state={
     progress:0
@@ -29,14 +30,14 @@ export default class App extends Component {
                  progress={this.state.progress}
               />
             <Routes>
-                <Route path='/' element={<News setProgress={this.setProgress}  key='general' pageSizes={this.pageSizes} country='us' category='general'/>} />
-                <Route path='/business' element={<News setProgress={this.setProgress}  key='business' pageSizes={this.pageSizes} country='us' category='business'/>} />
-                <Route path='/entertainment' element={<News setProgress={this.setProgress}  key='entertainment' pageSizes={this.pageSizes} country='us' category='entertainment'/>} />
-                <Route path='/general' element={<News setProgress={this.setProgress}  key='general' pageSizes={this.pageSizes} country='us' category='general'/>} />
-                <Route path='/health' element={<News setProgress={this.setProgress}  key='health' pageSizes={this.pageSizes} country='us' category='health'/>} />
-                <Route path='/science' element={<News setProgress={this.setProgress}  key='science' pageSizes={this.pageSizes} country='us' category='science'/>} />
-                <Route path='/sports' element={<News setProgress={this.setProgress}  key='sports' pageSizes={this.pageSizes} country='us' category='sports'/>} />
-                <Route path='/technology' element={<News setProgress={this.setProgress}  key='technology' pageSizes={this.pageSizes} country='us' category='technology'/>} />
+                <Route path='/' element={<News setProgress={this.setProgress}  key='general' pageSizes={this.pageSizes} apiKey={this.apiKey} country='us' category='general'/>} />
+                <Route path='/business' element={<News setProgress={this.setProgress}  key='business' pageSizes={this.pageSizes} apiKey={this.apiKey} country='us' category='business'/>} />
+                <Route path='/entertainment' element={<News setProgress={this.setProgress}  key='entertainment' pageSizes={this.pageSizes} apiKey={this.apiKey} country='us' category='entertainment'/>} />
+                <Route path='/general' element={<News setProgress={this.setProgress}  key='general' pageSizes={this.pageSizes} apiKey={this.apiKey} country='us' category='general'/>} />
+                <Route path='/health' element={<News setProgress={this.setProgress}  key='health' pageSizes={this.pageSizes} apiKey={this.apiKey} country='us' category='health'/>} />
+                <Route path='/science' element={<News setProgress={this.setProgress}  key='science' pageSizes={this.pageSizes} apiKey={this.apiKey} country='us' category='science'/>} />
+                <Route path='/sports' element={<News setProgress={this.setProgress}  key='sports' pageSizes={this.pageSizes} apiKey={this.apiKey} country='us' category='sports'/>} />
+                <Route path='/technology' element={<News setProgress={this.setProgress}  key='technology' pageSizes={this.pageSizes} apiKey={this.apiKey} country='us' category='technology'/>} />
             </Routes>
     
         </Router>
